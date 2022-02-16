2dcf11b29a87c0ca6e5bd9ad2a3a6e99a48a6e12
Qaisar Abbas <qaisar.abbas@flsl.pk>
2022-02-16 11:28:37 +0500

    Complete Roles and Permission

--- src/main/java/com/springsecuritydemo/controllers/UserController.java
+++ src/main/java/com/springsecuritydemo/controllers/UserController.java
@@ -1,46 +1,35 @@
 package com.springsecuritydemo.controllers;
 
-
 import com.springsecuritydemo.application.utility.JwtOutils;
 import com.springsecuritydemo.application.security.MyBCryptPasswordEncoder;
 import com.springsecuritydemo.domain.ApiResponse;
 import com.springsecuritydemo.domain.requests.AuthRequest;
 import com.springsecuritydemo.domain.requests.UserRegisterRequest;
-import com.springsecuritydemo.domain.response.AuthResponse;
 import com.springsecuritydemo.data.entities.User;
 import com.springsecuritydemo.data.persistance.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping
+@RequiredArgsConstructor
 public class UserController {
 
-    private com.springsecuritydemo.data.persistance.UserRepository UserRepository;
-    private MyBCryptPasswordEncoder myBCryptPasswordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private JwtOutils jwtOutils;
-
-    public UserController(UserRepository UserRepository,
-                          MyBCryptPasswordEncoder myBCryptPasswordEncoder) {
-        this.UserRepository = UserRepository;
-        this.myBCryptPasswordEncoder = myBCryptPasswordEncoder;
-    }
+    private final MyBCryptPasswordEncoder myBCryptPasswordEncoder;
+    private final UserRepository userRepository;
+    private final JwtOutils jwtOutils;
 
     @PostMapping("sign-in")
-    public AuthResponse signIn(@RequestBody AuthRequest authRequest) {
+    public ApiResponse signIn(@RequestBody AuthRequest authRequest) {
+
         User user = userRepository.findByUsername(authRequest.username);
         if (user != null && myBCryptPasswordEncoder.matches(authRequest.password, user.getPassword())) {
-            return new AuthResponse(jwtOutils.create(user));
+            return new ApiResponse(jwtOutils.create(user));
         }
-        return null;
+        return new ApiResponse("200", "Something Wrong!","null");
     }
 
     @PostMapping("sign-up")
